0e286e5973c976d82d2821f6fef6918a5b73a2d2
RyuGracie <iwanikremek@gmail.com>
2025-01-30 19:25:46 +0100

    final touch

--- Nook_Review/src/main_page.tsx
+++ Nook_Review/src/main_page.tsx
@@ -1,8 +1,16 @@
 export default function Main_Page() {
   return (
-    <div className="bg-sandy flex h-full w-full flex-col items-center justify-center p-4">
+    <div className="flex h-screen w-screen flex-col items-center justify-start bg-sandy p-4">
       <h1 className="text-2xl font-bold">Islands Reviewer</h1>
-      <p>Give a review to someone island, give the stars the meaning!</p>
+      <p className="mt-2 text-lg">
+        Welcome to Islands Reviewer! Here, you can share your thoughts and rate
+        the beautiful islands you've visited. Help others discover the best
+        spots by giving detailed reviews and meaningful star ratings.
+      </p>
+      <p className="mt-2 text-lg">
+        Whether it's a hidden gem or a popular destination, your insights can
+        make a difference. Dive in and start reviewing today!
+      </p>
     </div>
   );
 }
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Nook_Review
- Review Site for your Animal Crossing Island!
+
+Review Site for your Animal Crossing Island!
+
+## Prerequisites
+
+- Node.js (v16 or higher)
+- npm (v7 or higher)
+- Python (v3.8 or higher)
+- pip (v21 or higher)
+- SQLite (v3.35 or higher)
+
+## Frontend
+It hides in **Nook_Review** folder, it based on Vite+React 
+just install packages
+```sh
+npm i
+```
+and run dev
+```sh
+npm run dev
+```
+
+no fancy here, structure is TBA (Trash Based Architecture)
+
+## Backend
+it hides in **Review_Site** folder 
+it follows basic Django+DRF setup
+
+Run both for it to work
+It is very poorly made, i will give it that
